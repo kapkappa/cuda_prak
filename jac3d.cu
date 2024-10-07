f1ea68035e16d816378aaa47e22c264e601add04
kapkappa <roman.kupry@gmail.com>
2024-10-08 00:20:01 +0300

    update

--- jac3d.cu
+++ jac3d.cu
@@ -38,6 +38,25 @@ static inline size_t get_index(size_t i, size_t j, size_t k, size_t size) {
     return i * size * size + j * size + k;
 }
 
+
+double get_matrix_sum(double *M, size_t size) {
+    double sum = 0.0;
+
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size; j++) {
+            for (size_t k = 0; k < size; k++) {
+                sum += M[get_index(i, j, k, size)];
+            }
+        }
+    }
+
+    return sum;
+}
+
+/////////////////////////////////////////////////////////////////////
+
+
+
 template <uint32_t BLOCKSIZE>
 __device__ __forceinline__ void block_reduce_max(size_t i, double* data) {
     if (BLOCKSIZE > 512) { if (i < 512 && i + 512 < BLOCKSIZE) { data[i] = Max(data[i], data[i + 512]); } __syncthreads(); }
@@ -92,6 +111,64 @@ __global__ void jacobi(double *A, double *B, size_t NX, size_t NY, size_t NZ, do
 
     B[id] = (A[id - offset_x] + A[id - offset_y] + A[id - offset_z] + A[id + offset_x] + A[id + offset_y] + A[id + offset_z]) / 6.0;
 }
+
+
+template <uint32_t BLOCKSIZE>
+__global__ void get_eps(const double * __restrict__ A, const double * __restrict__ B, size_t NX, size_t NY, size_t NZ, double *eps_out) {
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x; // X-axis thread id
+    const size_t idy = blockIdx.y * blockDim.y + threadIdx.y; // Y-axis thread id
+    const size_t idz = blockIdx.z * blockDim.z + threadIdx.z; // Z-axis thread id
+
+    const size_t id = idx + idy * NX + idz * NX * NY;
+
+    const size_t thread_id = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;  // thread index in block
+    const size_t block_id = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;         // block index in grid
+
+    __shared__ double shared_eps[BLOCKSIZE];    //1-dimensional shared memory
+
+    double tmp = 0.0;
+
+    if (0 < idx && idx < (NX-1) && 0 < idy && idy < (NY-1) && 0 < idz && idz < (NZ-1)) {
+        tmp = fabs(B[id] - A[id]);
+    }
+
+    shared_eps[thread_id] = tmp;
+
+    __syncthreads();
+
+    block_reduce_max<BLOCKSIZE>(thread_id, shared_eps);
+
+    if (thread_id == 0) {
+        eps_out[block_id] = shared_eps[0];
+    }
+
+    return;
+}
+
+__global__ void update(const double * __restrict__ A, double *B, size_t NX, size_t NY, size_t NZ) {
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x; // X-axis thread id
+    const size_t idy = blockIdx.y * blockDim.y + threadIdx.y; // Y-axis thread id
+    const size_t idz = blockIdx.z * blockDim.z + threadIdx.z; // Z-axis thread id
+
+    const size_t id = idx + idy * NX + idz * NX * NY;
+
+    if (idx == 0 || idx >= (NX-1) || idy == 0 || idy >= (NY-1) || idz == 0 || idz >= (NZ-1)) {
+        return;
+    }
+
+    size_t offset_x = 1;
+    size_t offset_y = NX;
+    size_t offset_z = NX * NY;
+
+    B[id] = (A[id - offset_x] + A[id - offset_y] + A[id - offset_z] + A[id + offset_x] + A[id + offset_y] + A[id + offset_z]) / 6.0;
+
+    return;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+
 
 double solve(double *A, double *B, size_t size) {
     size_t NX = size, NY = size, NZ = size;
@@ -201,13 +278,16 @@ int main(int argc, char **argv) {
     double t1 = timer();
 
     for (int it = 1; it <= iters; it++) {
-
         if (drv == driver_t::GPU) {
-
-            jacobi<TOTAL_BLOCKSIZE><<<blocks_per_grid, threads_per_block>>>(d_A, d_B, NX, NY, NZ, eps_out);
+//            jacobi<TOTAL_BLOCKSIZE><<<blocks_per_grid, threads_per_block>>>(d_A, d_B, NX, NY, NZ, eps_out);
+            get_eps<TOTAL_BLOCKSIZE><<<blocks_per_grid, threads_per_block>>>(d_A, d_B, NX, NY, NZ, eps_out);
 
             thrust::device_ptr<double> eps_ptr = thrust::device_pointer_cast(eps_out);
             eps = *(thrust::max_element(eps_ptr, eps_ptr + grid_size));
+
+            CHECK_CUDA( cudaMemcpy(d_A, d_B, sizeof(double)*NX*NY*NZ, cudaMemcpyDeviceToDevice) )
+
+            update<<<blocks_per_grid, threads_per_block>>>(d_A, d_B, NX, NY, NZ);
 
         } else {
             eps = solve(h_A, h_B, size);
@@ -219,6 +299,13 @@ int main(int argc, char **argv) {
     }
 
     double t2 = timer();
+
+/*
+    if (drv == driver_t::GPU) {
+        CHECK_CUDA( cudaMemcpy(h_A, d_A, sizeof(double)*NX*NY*NZ, cudaMemcpyDeviceToHost) )
+    }
+    std::cout << "Matrix A sum: " << get_matrix_sum(h_A, size) << std::endl;
+*/
 
     free(h_A);
     free(h_B);
